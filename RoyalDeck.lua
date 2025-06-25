768bd4b61e0486a70a2e1590d1082bb62a8c2f47
SleepSpellia <skylersinghserwa@gmail.com>
2025-06-25 13:13:39 -0400

    Update RoyalDeck.lua

--- RoyalDeck.lua
+++ RoyalDeck.lua
@@ -578,7 +578,38 @@ SMODS.Consumable {
         }
     }
 }
-
+SMODS.Joker:take_ownership('raised_fist', {
+	calculate = function(self, card, context)
+		if context.individual and context.cardarea == G.hand and not context.end_of_round then
+			local temp_Mult, temp_Value = 15, 99999
+			local raised_card = nil
+			for i=1, #G.hand.cards do
+				if not SMODS.has_no_rank(G.hand.cards[i]) and temp_Value >= SMODS.Ranks[G.hand.cards[i].base.value].sort_nominal  then 
+					temp_Mult = G.hand.cards[i].base.nominal
+					temp_Value = SMODS.Ranks[G.hand.cards[i].base.value].sort_nominal
+					raised_card = G.hand.cards[i]
+				end
+			end
+			if raised_card == context.other_card then 
+				if context.other_card.debuff then
+					return {
+						message = localize('k_debuffed'),
+						colour = G.C.RED,
+						card = card,
+					}
+				else
+					return {
+						h_mult = 2*temp_Mult,
+						card = card,
+					}
+				end
+			else
+				--Return empty table to prevent vanilla calculation
+				return {}
+			end
+		end
+	end,
+}, true)
 
 
 
