0b18a720661d27fd81e1c548248acab782553c9b
SleepSpellia <skylersinghserwa@gmail.com>
2025-07-05 16:58:53 -0400

    Add files via upload

---
+++ atlas.lua
@@ -0,0 +1,145 @@
+SMODS.Atlas({
+    key = "RF_Cards_Cavalier",
+    path = "RF_Cards_Cavalier.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Cavalier_hc",
+    path = "RF_Cards_Cavalier_hc.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Vizier",
+    path = "RF_Cards_Vizier.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Vizier_hc",
+    path = "RF_Cards_Vizier_hc.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Sun",
+    path = "RF_Cards_Sun.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Sun_hc",
+    path = "RF_Cards_Sun_hc.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Un",
+    path = "RF_Cards_Un.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Un_hc",
+    path = "RF_Cards_Un_hc.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Fool",
+    path = "RF_Cards_Fool.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Fool_hc",
+    path = "RF_Cards_Fool_hc.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Merchant",
+    path = "RF_Cards_Merchant.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Merchant_hc",
+    path = "RF_Cards_Merchant_hc.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Glazier",
+    path = "RF_Cards_Glazier.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Glazier_hc",
+    path = "RF_Cards_Glazier_hc.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Barrister",
+    path = "RF_Cards_Barrister.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Barrister_hc",
+    path = "RF_Cards_Barrister_hc.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Pauper",
+    path = "RF_Cards_Pauper.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Tradesman_hc",
+    path = "RF_Cards_Tradesman_hc.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Tradesman",
+    path = "RF_Cards_Tradesman.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "RF_Cards_Pauper_hc",
+    path = "RF_Cards_Pauper_hc.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "Face Deck",
+    path = "Face Deck.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas({
+    key = "Face Deck",
+    path = "Face Deck.png",
+    px = 71,
+    py = 95,
+})
+SMODS.Atlas ({
+    key = "sleeves_Face",
+    path = "sleeves_Face.png",
+    px = 73,
+    py = 95
+})
+SMODS.Atlas({
+    key = 'modicon',
+    path = 'Icon.png',
+    px = 34,
+    py = 34
+})
+
